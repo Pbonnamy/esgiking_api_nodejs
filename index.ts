ef415a0496ea1ea2fe66826389de0d59e871a83c
Pierre <bonnamy.77@gmail.com>
2022-04-25 16:10:52 +0200

    hello world

--- index.ts
+++ index.ts
@@ -1,1 +1,14 @@
-console.log('ok')
+import {config} from "dotenv";
+config();
+import express from 'express';
+import {Request, Response} from "express";
+
+const app = express();
+
+app.get('/', function (req: Request, res: Response){
+    res.send("Hello world");
+})
+
+app.listen(process.env.PORT, function() {
+    console.log("Server started & listening on port " + process.env.PORT);
+})
