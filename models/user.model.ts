2d1822a388c2345b6fc0098ac8e81e1c64040f7f
Pierre <bonnamy.77@gmail.com>
2022-05-11 22:10:31 +0200

    dish middleware

--- models/user.model.ts
+++ models/user.model.ts
@@ -26,6 +26,12 @@ const userSchema = new Schema({
     versionKey: false
 });
 
+userSchema.methods.toJSON = function() {
+    const obj = this.toObject();
+    delete obj.password;
+    return obj;
+}
+
 export interface UserProps {
     _id: string;
     type: UserTypeProps
